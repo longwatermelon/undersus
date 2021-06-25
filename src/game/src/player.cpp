55f61c75cefa538162e6cdb94d0bc1cb1a6e15dc
longwatermelon <longwatermelon1@gmail.com>
2021-06-24 20:28:14 -0700

    clean up some code for readability

--- src/game/src/player.cpp
+++ src/game/src/player.cpp
@@ -1,4 +1,5 @@
 #include "player.h"
+#include "common.h"
 #include <iostream>
 #include <algorithm>
 #include <SDL_image.h>
@@ -22,28 +23,30 @@ void Player::move(const std::string& layout, int characters_per_line, const std:
     int xo = (m_velocity.x > 0 ? m_rect.w : 0);
     int yo = (m_velocity.y > 0 ? m_rect.h : 0);
 
-    int current_y = (int)(m_rect.y / (800 / characters_per_line));
-    int current_x = (int)(m_rect.x / (800 / characters_per_line));
+    int tile_size = 800 / characters_per_line;
 
-    int new_y = (int)((m_rect.y + m_velocity.y + yo) / (800 / characters_per_line));
-    int new_x = (int)((m_rect.x + m_velocity.x + xo) / (800 / characters_per_line));
+    int current_y = (int)(m_rect.y / tile_size);
+    int current_x = (int)(m_rect.x / tile_size);
 
-    if (std::find(solid_characters.begin(), solid_characters.end(), layout[current_y * characters_per_line + new_x]) == solid_characters.end())
+    int new_y = (int)((m_rect.y + m_velocity.y + yo) / tile_size);
+    int new_x = (int)((m_rect.x + m_velocity.x + xo) / tile_size);
+
+    if (!gui::common::exists(layout[current_y * characters_per_line + new_x], solid_characters))
     {
         m_rect.x += m_velocity.x;
     }
     else // inside of a solid block
     {
-        m_rect.x += (new_x * (800 / characters_per_line) + (xo == 0 ? 32 : 0 )) - (m_rect.x + xo);
+        m_rect.x += (new_x * tile_size + (xo == 0 ? tile_size : 0 )) - (m_rect.x + xo);
     }
     
-    if (std::find(solid_characters.begin(), solid_characters.end(), layout[new_y * characters_per_line + current_x]) == solid_characters.end())
+    if (!gui::common::exists(layout[new_y * characters_per_line + current_x], solid_characters))
     {
         m_rect.y += m_velocity.y;
     }
     else // inside of a solid block
     {
-        m_rect.y += (new_y * (800 / characters_per_line) + (yo == 0 ? 32 : 0)) - (m_rect.y + yo);
+        m_rect.y += (new_y * tile_size + (yo == 0 ? tile_size : 0)) - (m_rect.y + yo);
     }
 }
 
--- src/gui/src/common.h
+++ src/gui/src/common.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <SDL_ttf.h>
 
 
@@ -13,4 +15,10 @@ namespace gui::common
 
 
     SDL_Texture* render_text(SDL_Renderer* rend, const Font& font, const std::string& text, SDL_Color color);
+
+    template <typename T>
+    inline bool exists(T elem, const std::vector<T>& vec)
+    {
+        return std::find(vec.begin(), vec.end(), elem) != vec.end();
+    }
 }
