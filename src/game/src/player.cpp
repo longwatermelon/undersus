1e3e57956aba46c0a422c214dbdd20bfa740ca41
longwatermelon <longwatermelon1@gmail.com>
2021-06-24 20:15:55 -0700

    almost robust collision system

--- src/game/src/player.cpp
+++ src/game/src/player.cpp
@@ -29,9 +29,21 @@ void Player::move(const std::string& layout, int characters_per_line, const std:
     int new_x = (int)((m_rect.x + m_velocity.x + xo) / (800 / characters_per_line));
 
     if (std::find(solid_characters.begin(), solid_characters.end(), layout[current_y * characters_per_line + new_x]) == solid_characters.end())
+    {
         m_rect.x += m_velocity.x;
+    }
+    else // inside of a solid block
+    {
+        m_rect.x += (new_x * (800 / characters_per_line) + (xo == 0 ? 32 : 0 )) - (m_rect.x + xo);
+    }
     
     if (std::find(solid_characters.begin(), solid_characters.end(), layout[new_y * characters_per_line + current_x]) == solid_characters.end())
+    {
         m_rect.y += m_velocity.y;
+    }
+    else // inside of a solid block
+    {
+        m_rect.y += (new_y * (800 / characters_per_line) + (yo == 0 ? 32 : 0)) - (m_rect.y + yo);
+    }
 }
 
